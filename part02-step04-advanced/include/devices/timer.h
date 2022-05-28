c8c2efd2b390f4169a5fe1811ee10b1c1a5162d5
Ubuntu <ubuntu@ip-172-31-17-198.ap-northeast-2.compute.internal>
2022-05-28 16:10:26 +0000

    first commit

---
+++ part02-step04-advanced/include/devices/timer.h
@@ -0,0 +1,23 @@
+#ifndef DEVICES_TIMER_H
+#define DEVICES_TIMER_H
+
+#include <round.h>
+#include <stdint.h>
+
+/* Number of timer interrupts per second. */
+#define TIMER_FREQ 100  /* 1tick은 1ms로 정의되어 있고, 1ms마다 timer 인터럽트를 실행시켜 ticks 값을 1 증가시킴 */
+
+void timer_init (void);
+void timer_calibrate (void);
+
+int64_t timer_ticks (void);
+int64_t timer_elapsed (int64_t);
+
+void timer_sleep (int64_t ticks);
+void timer_msleep (int64_t milliseconds);
+void timer_usleep (int64_t microseconds);
+void timer_nsleep (int64_t nanoseconds);
+
+void timer_print_stats (void);
+
+#endif /* devices/timer.h */
